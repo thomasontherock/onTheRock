b46eaf59ee4bf043aa5db63707df85ad2c30c8ee
Thomas <thomasvandersteen2@gmail.com>
2024-02-19 15:36:08 +0100

    basic stuff

---
+++ Schaakbord.php
@@ -0,0 +1,76 @@
+<?php
+?>
+<div class="schaakbord">
+
+<div class="wit" id="a8"> </div>
+<div class="zwart" id="b8"> </div>
+<div class="wit" id="c8"> </div>
+<div class="zwart" id="d8"> </div>
+<div class="wit" id="e8"> </div>
+<div class="zwart" id="f8"> </div>
+<div class="wit" id="g8"> </div>
+<div class="zwart" id="h8"> </div>
+
+<div class="zwart" id="a7"> </div>
+<div class="wit" id="b7"> </div>
+<div class="zwart" id="c7"> </div>
+<div class="wit" id="d7"> </div>
+<div class="zwart" id="e7"> </div>
+<div class="wit" id="f7"> </div>
+<div class="zwart" id="g7"> </div>
+<div class="wit" id="h7"> </div>
+
+<div class="wit" id="a6"> </div>
+<div class="zwart" id="b6"> </div>
+<div class="wit" id="c6"> </div>
+<div class="zwart" id="d6"> </div>
+<div class="wit" id="e6"> </div>
+<div class="zwart" id="f6"> </div>
+<div class="wit" id="g6"> </div>
+<div class="zwart" id="h6"> </div>
+
+<div class="zwart" id="a5"> </div>
+<div class="wit" id="b5"> </div>
+<div class="zwart" id="c5"> </div>
+<div class="wit" id="d5"> </div>
+<div class="zwart" id="e5"> </div>
+<div class="wit" id="f5"> </div>
+<div class="zwart" id="g5"> </div>
+<div class="wit" id="h5"> </div>
+
+<div class="wit" id="a4"> </div>
+<div class="zwart" id="b4"> </div>
+<div class="wit" id="c4"> </div>
+<div class="zwart" id="d4"> </div>
+<div class="wit" id="e4"> </div>
+<div class="zwart" id="f4"> </div>
+<div class="wit" id="g4"> </div>
+<div class="zwart" id="h4"> </div>
+
+<div class="zwart" id="a3"> </div>
+<div class="wit" id="b3"> </div>
+<div class="zwart" id="c3"> </div>
+<div class="wit" id="d3"> </div>
+<div class="zwart" id="e3"> </div>
+<div class="wit" id="f3"> </div>
+<div class="zwart" id="g3"> </div>
+<div class="wit" id="h3"> </div>
+
+<div class="wit" id="a2"> </div>
+<div class="zwart" id="b2"> </div>
+<div class="wit" id="c2"> </div>
+<div class="zwart" id="d2"> </div>
+<div class="wit" id="e2"> </div>
+<div class="zwart" id="f2"> </div>
+<div class="wit" id="g2"> </div>
+<div class="zwart" id="h2"> </div>
+
+<div class="zwart" id="a1"> </div>
+<div class="wit" id="b1"> </div>
+<div class="zwart" id="c1"> </div>
+<div class="wit" id="d1"> </div>
+<div class="zwart" id="e1"> </div>
+<div class="wit" id="f1"> </div>
+<div class="zwart" id="g1"> </div>
+<div class="wit" id="h1"> </div>
+</div>
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php 
 
 ?>
+<html>
+<head>
+    <title> Schaakbord </title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+        include("schaakbord.php");
+    ?>
+    <button onclick="nextMove()"> > </button>
+    <div id="moveNr">1.0</div>
+    <div id="pgn"> pgnViewer</div>
+    <script src="javascript.js"></script>
+
+</body>
+</html>
